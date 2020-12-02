5586d1edb360a0d9720dda152da5b7dc14287b3d
LAlex <lalex@franky.local>
2020-12-02 08:36:25 +0100

    0.0.6

--- src/test.ts
+++ src/test.ts
@@ -1,7 +1,7 @@
 import { LM, LS } from ".";
 
-console.log(LS.changeCase("variable_name", LS.Case.SNAKE));
-console.log(LS.changeCase("6-th-floor", LS.Case.PASCAL));
-console.log(LS.guessCase("6th-floor"));
+// console.log(LS.changeCase("variable_name", LS.Case.SNAKE));
+// console.log(LS.changeCase("6-th-floor", LS.Case.PASCAL));
+// console.log(LS.guessCase("6th-floor"));
 
-console.log(LM.GOLDEN_RATIO);
+// console.log(LM.GOLDEN_RATIO);
