9bcdb7a50a4565347c9324a5ea39d9e8aba6c560
Alexandre LEGOUT <lalex@lalex.com>
2021-01-25 17:09:23 +0100

    Timer namespace (LT)

---
+++ src/debug/Timer.ts
@@ -0,0 +1,16 @@
+export namespace Timer {
+	export function timeSync<FT extends (...args: any[]) => any>(
+		cb: FT,
+		...args: Parameters<FT>
+	): { result: ReturnType<FT>; time: number } {
+		const start = new Date().valueOf();
+		const result = cb(...args);
+		return { result, time: new Date().valueOf() - start };
+    }
+    
+    export async function timeAsync<PT, FT extends (...args:any[]) => Promise<PT>>(cb : FT, ...args:Parameters<FT>) : Promise<{ result: PT; time: number }> {
+		const start = new Date().valueOf();
+		const result = await cb(...args);
+		return { result, time: new Date().valueOf() - start };
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,11 @@ import { MathUtils } from "./utils/MathUtils";
 import { ObjectUtils } from "./utils/ObjectUtils";
 import { StringUtils } from "./utils/StringUtils";
 import { ArrayUtils } from "./utils/ArrayUtils";
+import { Timer } from "./debug/Timer";
 
 export const LS = StringUtils;
 export const LM = MathUtils;
 export const LO = ObjectUtils;
 export const LA = ArrayUtils;
 export const LG = Logger;
+export const LT = Timer;
