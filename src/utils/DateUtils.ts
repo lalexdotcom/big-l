2009194dbd7625adb8c3d30529709cd3c726e578
Alexandre LEGOUT <lalex@lalex.com>
2021-02-09 09:37:47 +0100

    0.0.24

--- src/utils/DateUtils.ts
+++ src/utils/DateUtils.ts
@@ -3,9 +3,9 @@ import { format, parse } from "date-fns";
 export namespace DateUtils {
 	export const ISO_TZ_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSXX";
 
-	export function formatISOTZ(date: undefined) : undefined;
-	export function formatISOTZ(date: null) : null;
-	export function formatISOTZ(date: Date) : string;
+	export function formatISOTZ(date: undefined): undefined;
+	export function formatISOTZ(date: null): null;
+	export function formatISOTZ(date: Date): string;
 	export function formatISOTZ(date: Date | null | undefined): string | null | undefined {
 		if (!date) return date;
 		return format(date, "yyyy-MM-dd'T'HH:mm:ss.SSSXX");
