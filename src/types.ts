b1b3befe93da05baf0e6b1dfcb1c8841e12ce9d3
Alexandre LEGOUT <lalex@lalex.com>
2021-05-12 15:33:49 +0200

    Replace 0.5.0...

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,15 @@
-export namespace types {
-	export type StaticType<T, TT> = { new (...args: any[]): T } & TT; // Does it work?
-	export type ArrayElement<AT extends readonly unknown[]> = AT extends readonly (infer ElementType)[]
-		? ElementType
-		: never;
-	export type Basic = string | number | bigint | boolean | null | { [key: string]: Basic } | Basic[];
-	export namespace Basic {
-		export type Object = { [key: string]: Basic };
-	}
-	export type Async<T> = T | Promise<T>;
-	export type Promised<T> = T extends PromiseLike<infer U> ? Promised<U> : T;
+export type StaticType<T, TT> = { new(...args: any[]): T } & TT; // Does it work?
+
+export type ArrayElement<AT extends readonly unknown[]> = AT extends readonly (infer ElementType)[]
+	? ElementType
+	: never;
+
+// Basic types
+export type Basic = string | number | bigint | boolean | null | { [key: string]: Basic } | Basic[];
+export namespace Basic {
+	export type Object = { [key: string]: Basic };
 }
+
+// Promises
+export type Async<T> = T | Promise<T>;
+export type Promised<T> = T extends PromiseLike<infer U> ? Promised<U> : T;
