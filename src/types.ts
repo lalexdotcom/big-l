7b8abdc12ee4d6e45ad81d9ea4455b857585171e
Alexandre LEGOUT <lalex@lalex.com>
2021-05-18 16:46:05 +0200

    Added MapKey and MapElement types

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,11 @@
 export type StaticType<T, TT> = { new(...args: any[]): T } & TT; // Does it work?
 
-export type ArrayElement<AT extends readonly unknown[]> = AT extends readonly (infer ElementType)[]
-	? ElementType
+export type ArrayElement<AT extends readonly unknown[]> = AT extends readonly (infer ET)[]
+	? ET
 	: never;
+
+export type MapKey<MT> = MT extends Map<infer KT, any> ? KT : never;
+export type MapElement<MT> = MT extends Map<any, infer ET> ? ET : never;
 
 // Basic types
 export type Basic = string | number | bigint | boolean | null | { [key: string]: Basic } | Basic[];
