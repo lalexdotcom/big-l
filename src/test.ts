fbd4f35717ab851676aa94e1ea04b45d8f59209a
Alexandre LEGOUT <lalex@lalex.com>
2021-09-17 17:50:13 +0200

    Re-fix typing issue

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,6 @@
 import { ObjectUtils } from "./utils/ObjectUtils";
+import { ArrayUtils } from "./utils/ArrayUtils";
 
-const obj = { test: true, label: "MyObject" };
-const picked = ObjectUtils.pick(obj, ["label"]);
-console.log(picked);
+const obj1 = { store: { name: "ZZZZ" } }, obj2 = { store: { name: "AAAA" } };
+const sf = ArrayUtils.sortFunction("store.name");
+console.log(sf(obj1, obj2));
